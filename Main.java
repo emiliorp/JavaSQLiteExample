1765c8c0e280761b169ed982fc132aa4ac7961ec
Dmitry Vorobiev <web.dmitry.vorobiev@gmail.com>
2013-11-13 14:45:22 +0200

    working SQLite example

--- Main.java
+++ Main.java
@@ -1,5 +1,37 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException ignored) {
+        }
 
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+//            statement.executeUpdate("drop table if exists person");
+//            statement.executeUpdate("create table person (id integer, name string)");
+//            statement.executeUpdate("insert into person values(1, 'leo')");
+//            statement.executeUpdate("insert into person values(2, 'yui')");
+            ResultSet rs = statement.executeQuery("select * from person");
+            while (rs.next()) {
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
     }
 }
